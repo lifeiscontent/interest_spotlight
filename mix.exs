d4709a5d679ac546479fc5a217bcb5cc93c5a5d8
Peter Achieng <peterachieng01@gmail.com>
2025-12-17 15:04:01 +0300

    est: run coveralls automatically with mix test

--- mix.exs
+++ mix.exs
@@ -90,7 +90,7 @@ defmodule InterestSpotlight.MixProject do
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "coveralls"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["compile", "tailwind interest_spotlight", "esbuild interest_spotlight"],
       "assets.deploy": [
