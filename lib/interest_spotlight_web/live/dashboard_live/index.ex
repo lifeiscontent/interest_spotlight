efe67d2d595caaf8d56318d80f9a96058cd11052
Peter Achieng <peterachieng01@gmail.com>
2025-12-20 09:33:25 +0300

    feat: add dashboard page for authenticated users
    - Create dashboard LiveView at /dashboard
    - Redirect authenticated users from home to dashboard
    - Update signed_in_path to point to dashboard

---
+++ lib/interest_spotlight_web/live/dashboard_live/index.ex
@@ -0,0 +1,20 @@
+defmodule InterestSpotlightWeb.DashboardLive.Index do
+  use InterestSpotlightWeb, :live_view
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash} current_scope={@current_scope}>
+      <.header>
+        Dashboard
+        <:subtitle>Welcome back, {@current_scope.user.email}</:subtitle>
+      </.header>
+    </Layouts.app>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
--- lib/interest_spotlight_web/router.ex
+++ lib/interest_spotlight_web/router.ex
@@ -52,6 +52,7 @@ defmodule InterestSpotlightWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{InterestSpotlightWeb.UserAuth, :require_authenticated}] do
+      live "/dashboard", DashboardLive.Index, :index
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
     end
