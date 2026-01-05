4dd327b897cb22dd64af3b638f0056a401c0e382
Peter Achieng <peterachieng01@gmail.com>
2026-01-05 09:55:29 +0300

    Add nullable rating field to user_interests
    Allow users to optionally rate their interest level (e.g., 1-5) for
    better matching and prioritization of shared interests.

--- lib/interest_spotlight/interests/user_interest.ex
+++ lib/interest_spotlight/interests/user_interest.ex
@@ -5,13 +5,14 @@ defmodule InterestSpotlight.Interests.UserInterest do
   schema "user_interests" do
     belongs_to :user, InterestSpotlight.Accounts.User
     belongs_to :interest, InterestSpotlight.Interests.Interest
+    field :rating, :integer
 
     timestamps(type: :utc_datetime)
   end
 
   def changeset(user_interest, attrs) do
     user_interest
-    |> cast(attrs, [:user_id, :interest_id])
+    |> cast(attrs, [:user_id, :interest_id, :rating])
     |> validate_required([:user_id, :interest_id])
     |> unique_constraint([:user_id, :interest_id])
   end
